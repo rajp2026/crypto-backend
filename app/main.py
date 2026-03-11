0b8ab957a0c6fb1a28ba7c076624f7bc72f63dec
rajp2026 <rajprakshit2026@gmail.com>
2026-03-11 19:33:30 +0530

    added cores for frontend

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from app.database.session import get_db
 from app.api import auth_routes
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_routes.router)
 
