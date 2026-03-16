fef74e3eb771ab08dc91e6b659fdf9f879488c19
rajp2026 <rajprakshit2026@gmail.com>
2026-03-16 16:49:31 +0530

    added route for candle of coins

--- app/api/market_routes.py
+++ app/api/market_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.cache.redis_client import redis_client
 from app.services.market.market_ticker_service import MarketTickerService
+import httpx
 
 
 router = APIRouter(prefix="/market", tags=["market"])
@@ -33,3 +34,32 @@ def get_all_tickers():
 @router.get("/ticker/{symbol}")
 def get_ticker(symbol: str):
     return MarketTickerService.get_ticker(symbol)
+
+
+@router.get("/candles/{symbol}")
+async def get_candles(symbol: str):
+
+    url = "https://api.binance.com/api/v3/klines"
+
+    params = {
+        "symbol": symbol,
+        "interval": "1m",
+        "limit": 500
+    }
+
+    async with httpx.AsyncClient() as client:
+        res = await client.get(url, params=params)
+        data = res.json()
+
+    candles = []
+
+    for c in data:
+        candles.append({
+            "time": int(c[0] / 1000),
+            "open": float(c[1]),
+            "high": float(c[2]),
+            "low": float(c[3]),
+            "close": float(c[4]),
+        })
+
+    return candles
